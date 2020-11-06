5ebce2659eb6c5fe37344c7c1cb9621b83f51638
olamilekan-adeleke <43634204+Ola66@users.noreply.github.com>
2020-11-06 15:52:35 +0100

    added update in food

---
+++ food/food_api.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const router = express.Router();
+const datafiles = require('../data');
+const foodData = require('../food/food_data');
+const ilorinFoodData = require('../food/ilorin_food_data');
+
+
+router.get('/', (req, res)=>{
+    res.send('On Food Api');
+});
+
+router.get('/all_food_location', (req, res)=>{
+    res.send(foodData.all_food_location);
+});
+
+router.get('/ilorin_location', (req, res) =>{
+    console.log(req.query.main_area);
+    res.send(foodData.ilorin_location);
+});
+
+router.get('/main_areas', (req, res) =>{
+    res.send(foodData.main_area_names);
+});
+
+router.get('/price', (req, res)=>{
+    var areaName = req.query.area;
+    var location = req.query.location;
+    const data = getLoaction(location, areaName);
+
+    res.send(data);
+});
+
+///
+
+router.get('/unilorin', (req, res)=>{
+    res.send(datafiles.unilorin_area_name);
+});
+
+router.get('/unilag', (req, res)=>{
+    res.send(datafiles.unilag_area_name);
+});
+
+router.get('/lasu', (req, res)=>{
+    res.send(datafiles.Lasu_area_name);
+});
+
+router.get('/kwasu', (req, res)=>{
+    res.send(datafiles.kwasu_area_name);
+});
+
+router.get('/kwaraploy', (req, res)=>{
+    res.send(datafiles.kwaraPloy_area_name);
+});
+
+router.get('/funaab', (req, res)=>{
+    res.send(datafiles.funaab_area_name);
+});
+
+router.get('/al-hikmah', (req, res)=>{
+    res.send(datafiles.Al_Hikmah_area_name);
+});
+
+router.get('/food_delivery_info', (req, res)=>{
+    res.send(datafiles.food_delivery_price_info);
+});
+
+function getLoaction(location, area_name) {
+    if(location === 'ilorin'){
+        return getIlorinAreaPrice(area_name);
+    }
+}
+
+function getIlorinAreaPrice(area_name) {
+    if(area_name === 'taiwo'){
+        return ilorinFoodData.taiwo;
+    }else if(area_name === 'tanke'){
+        return ilorinFoodData.tanke;
+    }else{
+        return {};
+    }
+}
+
+
+module.exports = router;
---
+++ food/food_data.js
@@ -0,0 +1,19 @@
+const all_food_location = ['Kwara', 'Lagos', 'Detla'];
+
+const ilorin_location = [
+    'Unilorin PS', 'School Gas', 'Stella Maris', 'Oke Odo', 'Oko Oba', 'Tanke Ajanaku', 'Big Boy',
+    'Sanrab', 'Mark', 'NNPC', 'Tipper Garage ', 'Pipeline', 'F Division ', 'Tanke Junction ', 'Fate',
+    'Basin', 'onCampus', "Adeta","Adewale Estate","Olorunsogo", "Mandate", "Cottage", "Gerin Alimi",
+    'inside School', 'Gaa Akanbi', 'Olorunsoga', 'Post Office', 'Challenge', 'Maraba', 'Tawio', 'General',
+    'Oko Erin', 'Surulere', 'Al-hikmat', 'Asa Dam', 'Kwasu', 'Agric', 'Zango', 'Kulende', 'Oke Oyi', 'Oyun'
+];
+
+const main_area_names = [
+    'Tawio',
+    'Tanke',
+];
+
+
+exports.ilorin_location = ilorin_location;
+exports.main_area_names = main_area_names; 
+exports.all_food_location = all_food_location;
